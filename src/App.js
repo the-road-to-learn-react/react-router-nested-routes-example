1b18e99e0b6b0077d0d0c51b5b1dc5ccff6b9472
Robin Wieruch <wrobin@gmx.net>
2018-07-26 18:36:19 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,21 +1,75 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Fragment>
+          <Navigation />
+
+          <Switch>
+            <Route exact path="/faq" component={Faq} />
+            <Route path="/" component={Dashboard} />
+          </Switch>
+        </Fragment>
+      </Router>
     );
   }
 }
+
+const Navigation = () => (
+  <ul>
+    <li>
+      <Link to="/">Dashboard</Link>
+    </li>
+    <li>
+      <Link to="/faq">FAQ</Link>
+    </li>
+  </ul>
+);
+
+const Faq = () => <h1>FAQ</h1>;
+
+// Nested Routes
+const Dashboard = () => (
+  <div>
+    <h1>Dashboard</h1>
+
+    <Switch>
+      <Route exact path="/step-one" component={StepOne} />
+      <Route exact path="/step-two" component={StepTwo} />
+      <Route path="/" component={StepZero} />
+    </Switch>
+  </div>
+);
+
+// Declarative Navigation
+const StepZero = () => (
+  <div>
+    <h2>Status: Step Zero</h2>
+
+    <Link to="/step-one">Declarative navigation to Step 1</Link>
+  </div>
+);
+
+// Programmatic Navigation
+const StepOne = ({ history }) => (
+  <div>
+    <h2>Status: Step One</h2>
+
+    <button onClick={() => history.push('/step-two')} type="button">
+      Programmatic navigation to Step 2
+    </button>
+  </div>
+);
+
+const StepTwo = () => <h2>Status: Step Two</h2>;
 
 export default App;
